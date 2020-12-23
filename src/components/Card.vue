0781c98ede85ae94678986c2acae3fccd0de296f
Lukas Sorensen <lsorensen@sportsdigita.com>
2020-12-23 13:20:51 -0600

    add card interface

---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="playing-card">
+      <div class="top-left-info">
+          <div class="face">
+              {{faceShort}}
+          </div>
+      </div>
+      <div class="center-info">
+          
+      </div>
+      <div class="bottom-right-info">
+
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Card',
+    props: {
+        suit: String,
+        face: String,
+        faceShort: String,
+        value: Number
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/interfaces/card.interface.js
@@ -0,0 +1,44 @@
+export default class Card {
+    constructor(suit, face, value) {
+        this._suit = suit
+        this._face = face
+        this._value = value
+    }
+
+    get suit() {
+        return this._suit
+    }
+
+    get suitShort() {
+        return this._suit.slice(0,1).toUppercase()
+    }
+
+    get suitHtmlCode() {
+        switch(this._suit.toLowerCase()) {
+            case 'spades':
+            case 'spade':
+                return '&#9824;'
+            case 'hearts':
+            case 'heart':
+                return '&#9829;'
+            case 'clubs':
+            case 'club':
+                return '&#9827;'
+            case 'diamonds':
+            case 'diamond':
+                return '&#9830;'
+        }
+    }
+
+    get face() {
+        return this._face
+    }
+
+    get faceShort() {
+        return this._face.slice(0,1).toUppercase()
+    }
+
+    get value() {
+        return this._value
+    }
+}
